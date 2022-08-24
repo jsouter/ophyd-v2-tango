61657142c504c33d501e008a8c385989cd3dea1e
jsouter <james.souter@diamond.ac.uk>
2022-08-24 09:27:40 +0000

    sardemo v2 workflow

---
+++ image/startsard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+service mariadb start
+/usr/local/tango/bin/tango start
+yes y | Sardana demo & 
+# sleep 5 
+# printf "y\nDoor/demo/1\nsar_demo\n" | spock 
+# printf "y\nDoor/demo/1\n" | spock 
+
+# echo "sar_demo" | spock &
